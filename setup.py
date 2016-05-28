0a32de9eea14b9bebd3e3af26040af538fdef218
butjar <martin.t.fleischer@gmail.com>
2016-05-28 17:18:07 +0200

    Add setup.py https://docs.python.org/2/distutils/introduction.html#distutils-simple-example
    Add a simple setup script so pox can be installed with pip.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pox',
+      version='0.2.0',
+      description='Networking Software Platform',
+      url='http://www.noxrepo.org',
+      py_modules=['pox'],
+      )
